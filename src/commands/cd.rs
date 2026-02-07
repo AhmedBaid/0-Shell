60231e155a8664c15d9b87dde9c46192626d8418
AhmedBaid <baidahmed2005@gmail.com>
2026-02-07 18:10:43 +0100

    good

--- src/commands/cd.rs
+++ src/commands/cd.rs
@@ -5,6 +5,31 @@ pub fn command_cd(path: Vec<String>) {
         eprintln!("cd: too many arguments");
         return;
     }
+    
+    if path.is_empty() || path[0] == "~" {
+        if let Some(home) = env::var_os("HOME") {
+            match env::set_current_dir(&home) {
+                Ok(_) => {}
+                Err(e) => match e.kind() {
+                    ErrorKind::NotFound => {
+                        eprintln!("cd: No such file or directory: {}", "home");
+                    }
+                    ErrorKind::PermissionDenied => {
+                        eprintln!("cd: Permission denied: {}", "home");
+                    }
+                    ErrorKind::NotADirectory => {
+                        eprintln!("cd: Not a directory: {}", "home");
+                    }
+                    _ => {
+                        eprintln!("cd: error: {}", "home");
+                    }
+                },
+            }
+        } else {
+            eprintln!("cd: HOME environment variable not set");
+        }
+        return;
+    }
     if path[0] == "-" && path.len() == 1 {
         let oldpwd = match env::var("OLDPWD") {
             Ok(val) => val,
@@ -31,30 +56,6 @@ pub fn command_cd(path: Vec<String>) {
                     eprintln!("cd: error: {}", oldpwd);
                 }
             },
-        }
-        return;
-    }
-    if path.is_empty() || path[0] == "~" {
-        if let Some(home) = env::var_os("HOME") {
-            match env::set_current_dir(&home) {
-                Ok(_) => {}
-                Err(e) => match e.kind() {
-                    ErrorKind::NotFound => {
-                        eprintln!("cd: No such file or directory: {}", "home");
-                    }
-                    ErrorKind::PermissionDenied => {
-                        eprintln!("cd: Permission denied: {}", "home");
-                    }
-                    ErrorKind::NotADirectory => {
-                        eprintln!("cd: Not a directory: {}", "home");
-                    }
-                    _ => {
-                        eprintln!("cd: error: {}", "home");
-                    }
-                },
-            }
-        } else {
-            eprintln!("cd: HOME environment variable not set");
         }
         return;
     }
