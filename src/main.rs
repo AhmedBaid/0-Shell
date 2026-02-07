0db1d4a3097e1384d0b7a95cb4d7a16a52fcf97e
Marouane <enmaroine443@gmail.com>
2026-02-07 15:01:30 +0100

    add the arrow left and right

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 use std::env;
-use std::io::{ self, Write };
+use std::io::{self, Write, stdout};
 pub mod commands;
 pub mod helpers;
 use commands::cd::*;
 use helpers::parser::*;
 use helpers::print_banner::*;
 
-use crossterm::cursor::MoveToColumn;
-use crossterm::event::{ self, Event, KeyCode, KeyEventKind, KeyModifiers };
-use crossterm::execute;
-use crossterm::terminal::{ disable_raw_mode, enable_raw_mode, Clear, ClearType };
-
 use crate::helpers::check_quotes::quotes_balanced;
+use crossterm::cursor::{self, MoveToColumn};
+use crossterm::event::{self, Event, KeyCode, KeyEventKind, KeyModifiers};
+use crossterm::execute;
+use crossterm::terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode};
 
 const GREEN: &str = "\x1b[32m";
 const RESET: &str = "\x1b[0m";
@@ -23,9 +22,13 @@ fn main() -> io::Result<()> {
     let mut history: Vec<String> = vec![];
 
     loop {
-        let current_dir = env::current_dir().expect("Failed to get current working directory");
-        let prompt_text = format!("{GREEN}{}$ {RESET}", current_dir.display());
+        let current_dir = env::current_dir().expect("Failed to get CWD");
+        let dir_str = current_dir.display().to_string();
 
+        let prompt_len = dir_str.len() + 2;
+        let prompt_text = format!("{}{}$ {}", GREEN, dir_str, RESET);
+
+        execute!(stdout(), MoveToColumn(0), Clear(ClearType::CurrentLine))?;
         print!("{}", prompt_text);
         io::stdout().flush()?;
 
@@ -35,30 +38,61 @@ fn main() -> io::Result<()> {
         loop {
             if let Event::Key(key_event) = event::read()? {
                 if key_event.kind == KeyEventKind::Press {
+                    let (current_x, current_y) = cursor::position().unwrap();
+                    let cursor_idx = current_x as usize - prompt_len;
                     match key_event.code {
                         KeyCode::Char(c) => {
                             if key_event.modifiers.contains(KeyModifiers::CONTROL) && c == 'd' {
                                 print!("\r\n");
                                 disable_raw_mode()?;
                                 std::process::exit(0);
-                            } else if
-                                key_event.modifiers.contains(KeyModifiers::CONTROL) &&
-                                c == 'c'
+                            } else if key_event.modifiers.contains(KeyModifiers::CONTROL)
+                                && c == 'c'
                             {
                                 print!("\r\n");
                                 input_buffer.clear();
                                 break;
                             }
-                            input_buffer.push(c);
-                            print!("{}", c);
+                            if cursor_idx >= input_buffer.len() {
+                                input_buffer.push(c);
+                            } else {
+                                input_buffer.insert(cursor_idx, c);
+                            }
+
+                            execute!(
+                                stdout(),
+                                cursor::MoveToColumn(prompt_len as u16),
+                                Clear(ClearType::UntilNewLine)
+                            )?;
+                            print!("{}", input_buffer);
+
+                            execute!(
+                                stdout(),
+                                cursor::MoveTo((prompt_len + cursor_idx + 1) as u16, current_y)
+                            )?;
                             io::stdout().flush()?;
                         }
 
                         KeyCode::Backspace => {
                             if !input_buffer.is_empty() {
-                                input_buffer.pop();
-                                print!("\x08 \x08");
-                                io::stdout().flush()?;
+                                if cursor_idx > 0 {
+                                    input_buffer.remove(cursor_idx - 1);
+
+                                    execute!(
+                                        stdout(),
+                                        cursor::MoveToColumn(prompt_len as u16),
+                                        Clear(ClearType::UntilNewLine)
+                                    )?;
+                                    print!("{}", input_buffer);
+                                    execute!(
+                                        stdout(),
+                                        cursor::MoveTo(
+                                            (prompt_len + cursor_idx - 1) as u16,
+                                            current_y
+                                        )
+                                    )?;
+                                    io::stdout().flush()?;
+                                }
                             }
                         }
 
@@ -105,6 +139,18 @@ fn main() -> io::Result<()> {
                                 )?;
                                 print!("{}{}", prompt_text, input_buffer);
                                 io::stdout().flush()?;
+                            }
+                        }
+                        KeyCode::Left => {
+                            if cursor_idx > 0 {
+                                execute!(stdout(), cursor::MoveTo(current_x - 1, current_y))
+                                    .unwrap();
+                            }
+                        }
+                        KeyCode::Right => {
+                            if cursor_idx < input_buffer.len() {
+                                execute!(stdout(), cursor::MoveTo(current_x + 1, current_y))
+                                    .unwrap();
                             }
                         }
                         _ => {}
